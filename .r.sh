301532734628e597b079c504ac801dbcdc6e2134
AbyssWalker <63506758+AbyssExploit@users.noreply.github.com>
2020-10-23 04:06:36 +0700

    Create .r.sh

---
+++ .r.sh
@@ -0,0 +1,45 @@
+#!/system/xbin/bash
+#abysswalker
+#recodelu_memang_nub
+
+line="\e[1;31m[@]\e[0m"
+ine="\e[1;31m[ ∆ ]\e[0m"
+
+clear
+toilet --metal -f future " Welcome Back"
+sleep 1
+toilet --metal -f future " To"
+sleep 1
+toilet --metal -f future " Collection"
+sleep 1
+toilet --metal -f future " Cyber Tools"
+sleep 1
+clear
+toilet --metal -f small " Abyss Walker"
+sleep 1
+clear
+
+echo -e "$line Please Wait"
+echo -e "$line Checking Server"
+sleep 1
+echo -e "." | lolcat
+echo -e ".." | lolcat
+echo -e "..." | lolcat
+echo -e "...." | lolcat
+echo -e "....." | lolcat
+echo ""
+echo -e "$line All Tools Up To Date"
+echo -e "$line DONE!"
+echo -e "$line Enjoy!"
+sleep 4
+clear
+echo -e "$ine Warning"
+echo -e "$ine Tools Ini Berisi Alat Alat Hacking"
+echo -e "$ine Saya Tidak Bertanggung Jawab Atas"
+echo -e "$ine Kerusakan Yang Di Timbulkan Tools"
+echo -e "$ine Ini Atau Penggunaan Illegal Yang"
+echo -e "$ine Melanggar Hukum!"
+echo -e "$ine Do With You Own Risk"
+echo -e "$ine ~ ABYSS WALKER ~"
+sleep 10
+clear
